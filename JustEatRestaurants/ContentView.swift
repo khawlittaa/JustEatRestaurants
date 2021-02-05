c32f0bd6b21431c7658748aee877cf45de6de718
khaoula.hafsia <khaoula.hafsia@proxym-it.com>
2021-02-05 17:41:13 +0100

    setting up project

---
+++ JustEatRestaurants/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JustEatRestaurants
+//
+//  Created by khaoula hafsia on 5/2/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
